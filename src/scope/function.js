e66359bb45872a8a36b7d5cf370040d5beb3cf68
Javi Felices <javifelices@gmail.com>
2021-08-12 18:07:44 +0200

    Añadir Function Scope

---
+++ src/scope/function.js
@@ -0,0 +1,19 @@
+const fruits = () => {
+    var fruit = 'apple';
+    console.log("🚀 ~ file: function.js ~ fruit", fruit);
+};
+
+fruits();
+// console.log("🚀 ~ file: function.js ~ fruit", fruit);
+
+const anotherFunction = () => {
+    var x = 1;
+    var x = 2;
+    let y = 1;
+    // let y = 2;
+    y = 2;
+    console.log("🚀 anotherFunction ~ x", x);
+    console.log("🚀 anotherFunction ~ y", y);
+};
+
+anotherFunction();
